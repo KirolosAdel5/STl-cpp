7525e8bfb9630d989eb42f8d847bb18284a5eadd
keroo-adel <kerooadel5@gmail.com>
2021-01-31 23:42:46 +0200

    updata

--- code.cpp
+++ code.cpp
@@ -8,5 +8,13 @@ int main()
 		//pair (data_type1, data_type2) Pair_name
     pair<int, char> PAIR1;
 	
-	return 0;
+    PAIR1.first = 100;
+    PAIR1.second = 'G';
+ 
+    cout << PAIR1.first << " "<< PAIR1.second << endl;
+	//output is:
+	/**************************
+	 100 G
+	**************************/
+    return 0;
 }
